aec6015006c4cccb10adc70bfde96b8939312f3e
Stefan Ginev <stefan.ginev@toptal.com>
2020-05-24 22:09:32 +0300

    switch to abs; clean up; main now 14 lines

--- main.ts
+++ main.ts
@@ -1,27 +1,14 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/abc/mod.ts";
 import { resolveQuery } from "./lib/data.ts";
 
 const { data } = await resolveQuery( new TextDecoder('utf8').decode( await Deno.readFile( "test.gql" ) ) )
 console.log( data );
 
 const app = new Application();
-const router = new Router();
 
-router.post( `/`, async ({ request, response }) => {
-  const body = await request.body();
-  const query = body.value.query;
-  try {
-    const result = await resolveQuery( query );
-    //console.log({ query, result });
-    response.status = 200;
-    response.body = result;
-  } catch ( error ) {
-    response.status = 500;
-    response.body = { errors: [ error ], outsideGraphQL: true }
-  }
+app.post( `/`, async c => {
+  const { query } = await c.body<{query:string}>();
+  return await resolveQuery( query );
 } )
 
-app.use( router.routes() );
-app.use( router.allowedMethods() );
-
-await app.listen(`0.0.0.0:7999`);
+app.start( { port:7999 } );
